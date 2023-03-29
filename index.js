0b07a460148a36fc943036aa6d46a184bd0c5959
Anthony Lotson <focus_and_finish@yahoo.com>
2023-03-28 21:24:09 -0400

    Lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+//let activity;
+saturdayFun();
+//saturdayFun("bathe my dog!")
+
+function mondayWork(workLocation = "go to the office") {
+    return `This Monday, I will ${workLocation}.`
+}
+mondayWork();
+
+//function wrapAdjective(low = "||special||") {
+/*function wrapAdjective(low = "||dedicated||") {
+    return function (high = "*"){
+        return `You are *${high}*!`
+    }
+}
+wrapAdjective();
+*/
+function wrapAdjective (flair ="*"){
+    return function inner(workerDescription = "special"){
+        //return `You are *${inner}*!`
+        return `You are ${flair}${workerDescription}${flair}!`
+    }
+}
+wrapAdjective();
